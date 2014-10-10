8a79ded2a275b17acf989512c1d169843ed89ec8
Jeroen Lammerts <jeroenlammerts@gmail.com>
2014-10-10 12:12:12 +0200

    setup project

---
+++ app/src/main/java/com/example/jeroenlammerts/virtualtherapist/ChatBubbleActivity.java
@@ -0,0 +1,108 @@
+package com.example.jeroenlammerts.virtualtherapist;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.DataSetObserver;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnKeyListener;
+import android.widget.AbsListView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class ChatBubbleActivity extends Activity {
+    private static final String TAG = "ChatActivity";
+
+    private ChatArrayAdapter chatArrayAdapter;
+    private ListView listView;
+    private EditText chatText;
+    private Button buttonSend;
+
+    Intent intent;
+    private boolean side = true;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent i = getIntent();
+        setContentView(R.layout.activity_chat);
+
+        buttonSend = (Button) findViewById(R.id.buttonSend);
+
+        listView = (ListView) findViewById(R.id.listView1);
+
+        chatArrayAdapter = new ChatArrayAdapter(getApplicationContext(), R.layout.activity_chat_singlemessage);
+        listView.setAdapter(chatArrayAdapter);
+
+        chatText = (EditText) findViewById(R.id.chatText);
+        chatText.setOnKeyListener(new OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    return parseQuestion(chatText.getText().toString());
+                }
+                return false;
+            }
+        });
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                parseQuestion(chatText.getText().toString());
+            }
+        });
+
+        listView.setTranscriptMode(AbsListView.TRANSCRIPT_MODE_ALWAYS_SCROLL);
+        listView.setAdapter(chatArrayAdapter);
+
+        //to scroll the list view to bottom on data change
+        chatArrayAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                listView.setSelection(chatArrayAdapter.getCount() - 1);
+            }
+        });
+
+        this.sendChatMessage("Hallo, waar kan ik u mee helpen?");
+
+        RestAdapter restAdapter = new RestAdapter.Builder()
+                .setEndpoint("http://127.0.0.1/api")
+                .build();
+
+        VirtualTherapistService vtService = restAdapter.create(VirtualTherapistService.class);
+
+        vtService.addQuestion("Ben ik te dik?", new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+
+            }
+        });
+
+
+    }
+
+    private boolean sendChatMessage(String msg){
+        chatArrayAdapter.add(new ChatMessage(side, msg));
+        chatText.setText("");
+        side = !side;
+        return true;
+    }
+
+    private boolean parseQuestion(String question) {
+        this.sendChatMessage(question);
+        this.sendChatMessage("Ik begrijp u nog niet helaas...");
+        return true;
+    }
+
+}
