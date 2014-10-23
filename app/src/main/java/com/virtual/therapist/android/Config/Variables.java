4ef1c694aa826ed934e934f0d99909e1712deba2
Vasco de Krijger <vascokiller@live.nl>
2014-10-23 11:37:10 +0200

    Layout van de app aangepast, als er feedback is hoor ik het graag :)

--- app/src/main/java/com/virtual/therapist/android/Config/Variables.java
+++ app/src/main/java/com/virtual/therapist/android/Config/Variables.java
@@ -5,6 +5,6 @@ package com.virtual.therapist.android.Config;
  */
 public class Variables
 {
-    public static final String API_URL          = "http://94.212.100.11:9000/api/";
-    public static final String WEBSOCKET_URL    = "ws://94.212.100.11:9000/server";
+    public static final String API_URL          = "http://145.37.63.135:9000/api/";
+    public static final String WEBSOCKET_URL    = "ws://145.37.63.135:9000/server";
 }
