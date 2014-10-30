06b77b876cffbbe54044fab42c4e54aa480a18a4
Vasco de Krijger <vascokiller@live.nl>
2014-10-30 14:29:44 +0100

    added server ip

--- app/src/main/java/com/virtual/therapist/android/Config/Variables.java
+++ app/src/main/java/com/virtual/therapist/android/Config/Variables.java
@@ -5,7 +5,8 @@ package com.virtual.therapist.android.Config;
  */
 public class Variables
 {
-    private static final String IP              = "145.37.95.30:9000";
+    private static final String IP            = "145.37.95.30:9000";
+//    private static final String IP              = "188.226.243.85:9000";//server
     public static final String API_URL          = "http://" + IP + "/api/";
     public static final String WEBSOCKET_URL    = "ws://" + IP + "/server";
 }
