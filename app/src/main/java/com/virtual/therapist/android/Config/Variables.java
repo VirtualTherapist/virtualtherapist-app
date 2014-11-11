6630bbc3fd0b87860b835dffe9a0b32c430d68e4
Jeroen Lammerts <jeroenlammerts@gmail.com>
2014-11-11 11:29:07 +0100

    server ip

--- app/src/main/java/com/virtual/therapist/android/Config/Variables.java
+++ app/src/main/java/com/virtual/therapist/android/Config/Variables.java
@@ -2,8 +2,7 @@ package com.virtual.therapist.android.Config;
 
 public class Variables
 {
-//    private static final String IP              = "188.226.243.85:9000";
-    private static final String IP              = "145.37.89.70:9000";
+    private static final String IP              = "149.210.178.219";
     public static final String API_URL          = "http://" + IP + "/api/";
     public static final String WEBSOCKET_URL    = "ws://" + IP + "/server";
 }
