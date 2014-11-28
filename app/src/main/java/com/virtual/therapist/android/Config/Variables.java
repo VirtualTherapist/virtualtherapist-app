9e6cbb96e5b40cab6c82c3c5d65fffd69d62c938
Jeroen Lammerts <jeroenlammerts@gmail.com>
2014-11-28 12:16:06 +0100

    latest apk

--- app/src/main/java/com/virtual/therapist/android/Config/Variables.java
+++ app/src/main/java/com/virtual/therapist/android/Config/Variables.java
@@ -2,7 +2,7 @@ package com.virtual.therapist.android.Config;
 
 public class Variables
 {
-    private static final String IP              = "149.210.178.219:9090";
+    private static final String IP              = "vtr.arankadol.nl:8999";
     public static final String API_URL          = "http://" + IP + "/api/";
     public static final String WEBSOCKET_URL    = "ws://" + IP + "/server";
 }
